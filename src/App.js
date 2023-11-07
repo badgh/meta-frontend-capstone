b0343b9179c69d67811248d7fd1cb965f684e219
bad <tbad2009@gmail.com>
2023-11-07 16:24:35 -0700

    Table booking system

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Main from "./components/Main";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import Reservations from "./pages/Reservations";
+import Bookings from "./pages/Bookings";
+import ConfirmedBooking from "./pages/ConfirmedBooking";
 
 const App = () => {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/reservations" element={<Reservations />} />
-      </Routes>
+      <Main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/reservations" element={<Bookings />} />
+          <Route
+            path="/reservation-confirmation"
+            element={<ConfirmedBooking />}
+          />
+        </Routes>
+      </Main>
       <Footer />
     </>
   );
---
+++ src/pages/ConfirmedBooking.js
@@ -0,0 +1,5 @@
+const ConfirmedBooking = () => {
+  return <p>Your reservation has been placed.</p>;
+};
+
+export default ConfirmedBooking;
