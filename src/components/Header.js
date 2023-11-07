bba6babfc671e5762bf524dbb8858149fa9ac723
bad <tbad2009@gmail.com>
2023-11-07 12:13:03 -0700

    Amend core components and assets

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Logo from "../assets/Logo.svg";
+import Nav from "./Nav";
 
 const Header = () => {
   return (
     <header>
       <img src={Logo} alt="Little Lemon Logo" />
+      <Nav></Nav>
     </header>
   );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,23 @@
 import React from "react";
 
+const navLinks = [
+  { anchor: "", name: "Home" },
+  { anchor: "", name: "About" },
+  { anchor: "", name: "Menu" },
+  { anchor: "", name: "Reservations" },
+  { anchor: "", name: "Order Online" },
+  { anchor: "", name: "Login" },
+];
+
 const Nav = () => {
   return (
     <nav>
       <ul>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
+        {navLinks.map((navLink, index) => (
+          <li key={index}>
+            <a href={navLink.anchor}>{navLink.name}</a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
